b659f7e346438fbb3eab69de8bcfe12b55d14fad
beom205 <beom205@gmail.com>
2020-03-19 16:10:51 +0900

    결과 페이지 리스트 생성 및 클릭 이벤트 설정 완료
    * 이미지 여러장을 등록하면 분석 후 결과페이지에서 그 수만큼 리스트 생성
    * 그 후 클릭 이벤트 설정
    * 이제 테스트 서버에서 모델 추가하여 동작 확인
    * 프론트에서는 로딩화면 추가하기

--- flask_upload.py
+++ flask_upload.py
@@ -30,29 +30,35 @@ def main_page():
 def upload_imgs():
   if request.method == 'POST':
 
-    img = request.files['file']
+    #img = request.files['files']
+    imgs = request.files.getlist("file[]")
+    # imgs = request.files.to_dict()
+    print("imgs : ",imgs)
+    #이미지가 저장된 전체 경로
+    resultDataList = []
 
-    #저장할 경로 + 파일명
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
-    img.save(full_filename)
+    for img in imgs:
+      # print("img : ",img)
+      # print("filename : ",img.filename)
+      #저장할 경로 + 파일명
+      full_filename = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
+      full_filename = full_filename.replace('\\','/')
+      img.save(full_filename)
 
-    #폐렴 분석
-    #prob = PredictModel.predict_using_path(full_filename)
+      #모델 없이 테스트 용
+      prob = 0.984324
+      #print("#결과 : ",prob)
+      #폐렴의심 여부 (1이면 폐렴, 0이면 정상)
+      isPneumonia = 1 if prob > 0.5 else 0
+      #폐렴검출 정확도
+      acc = int(prob*100)
+      #응답할 결과 데이터
+      resultDataList.append({'imgs' : full_filename,
+                   'acc' : acc, 'isPneumonia' : isPneumonia})
 
-    #모델 없이 테스트 용
-    prob = 0.984324
+    print("### resultDataList : ", resultDataList)
 
-    #print("#결과 : ",prob)
-    #폐렴의심 여부 (1이면 폐렴, 0이면 정상)
-    isPneumonia = 1 if prob > 0.5 else 0
-    #폐렴검출 정확도
-    acc = int(prob*100)
-    #응답할 결과 데이터
-    resultData = {'imgs' : full_filename, 'acc' : acc, 'isPneumonia' : isPneumonia}
-    
-    #print("#resultData : ",resultData)
-    #return render_template("result.html", resultData = resultData)
-    return render_template("result3.html", resultData = resultData)
+    return render_template("result3.html", resultDataList = resultDataList)
 
 @app.route('/text', methods=['GET','POST'])
 def upload_text():
--- static/js/result.js
+++ static/js/result.js
@@ -1,21 +1,26 @@
-//이미지에 이벤트 핸들러 넣기-> 1. img src가져오기 2. class active 넣어서 css바꾸기 3.다른 class는 active 지우기
-var lists = document.querySelectorAll(".list li");
-var big_image = document.querySelector("#big img");
-var currentimg;
+//이미지에 이벤트 핸들러 넣기-> 1. img src가져오기 2. class active 넣어서 css바꾸기 3.다른 class는 active 지우기  
+function setPhotoListEvent(){
+  var lists = document.querySelectorAll(".list li");
+  var big_image = document.querySelector("#big img");
+  var currentimg;
 
-for (var i=0 ; lists.length ;i++){
-lists[i].addEventListener('click',clickHandler);
-}
+  for (var i=0 ; i<lists.length ;i++){
+    lists[i].addEventListener('click',clickHandler);
+  }
+  
+  //초기 첫번째 이미지 클릭
+  lists[0].click();
 
-function clickHandler(e) {
-if(currentimg){
-    currentimg.classList.remove('active');
-}
-this.classList.add('active');
-currentimg=this;
-var img=this.querySelector('img');
-var img_src=img.getAttribute('src');
-big_image.setAttribute("src",img_src);
-click_img = document.querySelector("#big p");
-click_img.style.display="none";
+  function clickHandler(e) {
+    if(currentimg){
+        currentimg.classList.remove('active');
+    }
+    this.classList.add('active');
+    currentimg=this;
+    var img=this.querySelector('img');
+    var img_src=img.getAttribute('src');
+    big_image.setAttribute("src",img_src);
+    click_img = document.querySelector("#big p");
+    click_img.style.display="none";
+  }
 }
